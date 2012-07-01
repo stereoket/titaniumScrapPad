defaa21792dfc7d638a5adb3d39a52c1d0c5adc1
Joe Lee <theycallmejoe@gmail.com>
2012-07-01 21:02:17 +0100

    working version of the slider

--- Resources/app.js
+++ Resources/app.js
@@ -13,16 +13,6 @@ var win1 = Titanium.UI.createWindow({
     backgroundColor:'#C7C7C7'
 });
 
-// var label1 = Titanium.UI.createLabel({
-	// color:'#999',
-	// text:'I am Window 1',
-	// font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	// textAlign:'center',
-	// width:'auto'
-// });
-// 
-// win1.add(label1);
-
 var imageslider = Titanium.UI.createScrollView({
     contentWidth:'auto',
     contentHeight:80,
